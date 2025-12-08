9db09687ad40935670820d8ae4c9c0a47402f95c
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-12-08 11:07:30 +0700

    feat: Repository & UnitOfWork for EFCore, IRepository & IUnitOfWork

---
+++ src/backend/BuildingBlocks/BuildingBlocks.Core/Infrastructure/EFCore/BaseRepositoryEFcore.cs
@@ -0,0 +1,45 @@
+﻿using BuildingBlocks.Core.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BuildingBlocks.Core.Infrastructure.EFCore
+{
+    public class BaseRepositoryEFcore<T, TContext> : IRepository<T>
+         where T : class
+         where TContext : DbContext
+    {
+        protected readonly TContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public BaseRepositoryEFcore(TContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+            => await _dbSet.Where(predicate).ToListAsync();
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
---
+++ src/backend/BuildingBlocks/BuildingBlocks.Core/Infrastructure/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Core.Infrastructure.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
