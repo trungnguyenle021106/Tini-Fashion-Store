6a06414671c84515b62748887835a73410842574
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-12-14 11:10:28 +0700

    feat(building-blocks): SwaggerExtension

---
+++ src/backend/BuildingBlocks/BuildingBlocks.Core/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+
+namespace BuildingBlocks.Core.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddCustomSwagger(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen(options =>
+            {
+                var serviceName = configuration["Swagger:Title"] ?? "My Microservice API";
+                var version = configuration["Swagger:Version"] ?? "v1";
+
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = serviceName,
+                    Version = version,
+                    Description = $"API Documentation for {serviceName}"
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseCustomSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            return app;
+        }
+    }
+}
