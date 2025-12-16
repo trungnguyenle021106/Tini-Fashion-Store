3239044bd96ad93eb15c2e4ed27affcda882eadf
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-12-16 09:25:37 +0700

    feat(building-blocks): PainatedExtensions

---
+++ src/backend/BuildingBlocks/BuildingBlocks.Core/CQRS/PaginatedResult.cs
@@ -0,0 +1,18 @@
+﻿namespace BuildingBlocks.Core.CQRS
+{
+    public class PaginatedResult<T>
+    {
+        public int PageIndex { get; }
+        public int PageSize { get; }
+        public long Count { get; } 
+        public IEnumerable<T> Data { get; } 
+
+        public PaginatedResult(int pageIndex, int pageSize, long count, IEnumerable<T> data)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            Count = count;
+            Data = data;
+        }
+    }
+}
---
+++ src/backend/BuildingBlocks/BuildingBlocks.Core/Extensions/PaginationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlocks.Core.CQRS;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuildingBlocks.Core.Extensions
+{
+    public static class PaginationExtensions
+    {
+        public static async Task<PaginatedResult<TDestination>> ToPaginatedListAsync<TEntity, TDestination>(
+            this IQueryable<TEntity> queryable,
+            int pageNumber,
+            int pageSize,
+            CancellationToken cancellationToken = default)
+            where TDestination : class
+        {
+            var count = await queryable.LongCountAsync(cancellationToken);
+
+            var items = await queryable
+                .ProjectToType<TDestination>()
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            return new PaginatedResult<TDestination>(pageNumber, pageSize, count, items);
+        }
+    }
+}
