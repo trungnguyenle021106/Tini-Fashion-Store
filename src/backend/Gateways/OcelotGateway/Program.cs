be8aa70c15ff3cd121935dd074beff165fab481b
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-12-27 09:09:14 +0700

    feat(gateway): Config gateway for API

--- src/backend/Gateways/OcelotGateway/Program.cs
+++ src/backend/Gateways/OcelotGateway/Program.cs
@@ -1,3 +1,28 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", b =>
+    {
+        b.WithOrigins("http://localhost:3000") 
+         .AllowAnyMethod()
+         .AllowAnyHeader()
+         .AllowCredentials(); 
+    });
+});
+
+builder.Services.AddOcelot();
+
 var app = builder.Build();
+
+app.UseCors("CorsPolicy");
+
+await app.UseOcelot();
+
 app.Run();
